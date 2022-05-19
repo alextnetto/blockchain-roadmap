6780a8fa5acc02eabcf613a975af88b452e77a87
Alexandro T. Netto <alex.t.netto@gmail.com>
2022-05-18 23:32:04 -0300

    Visibilidade aula

---
+++ curso-solidity/6 - Visibilidade.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+contract Visibilidade {
+    // Variáveis
+    // public, private, internal, external
+    // Funções
+    // public, private, internal, external
+}
