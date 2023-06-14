8809766c739e3c47a0f7ec6c859d350dd365caed
Alexandro T. Netto <alex.t.netto@gmail.com>
2023-06-14 16:18:59 -0300

    add Chainlink Automation

---
+++ curso-solidity/chainlink/Automation.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+interface AutomationCompatibleInterface {
+    function checkUpkeep(
+        bytes calldata checkData
+    ) external returns (bool upkeepNeeded, bytes memory performData);
+
+    function performUpkeep(bytes calldata performData) external;
+}
+
+contract DataFeed is AutomationCompatibleInterface {
+    bool public mustBeTrue;
+
+    constructor() {
+        mustBeTrue = true;
+    }
+
+    function checkUpkeep(
+        bytes calldata checkData
+    )
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory performData)
+    {
+        return (!mustBeTrue, checkData);
+    }
+
+    function performUpkeep(bytes calldata performData) external override {
+        mustBeTrue = true;
+    }
+
+    function triggerAutomation() external {
+        mustBeTrue = false;
+    }
+}
